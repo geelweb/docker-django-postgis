7dabe7ea0908102158bae5260aa33c3351f0f32e
Guillaume Luchet <guillaume@geelweb.org>
2015-07-21 23:24:08 +0200

    Add worldborder mapping

--- geodjango/world/models.py
+++ geodjango/world/models.py
@@ -23,3 +23,20 @@ class WorldBorder(models.Model):
     # Returns the string representation of the model.
     def __unicode__(self):
         return self.name
+
+# Auto-generated `LayerMapping` dictionary for WorldBorder model
+worldborder_mapping = {
+    'fips' : 'FIPS',
+    'iso2' : 'ISO2',
+    'iso3' : 'ISO3',
+    'un' : 'UN',
+    'name' : 'NAME',
+    'area' : 'AREA',
+    'pop2005' : 'POP2005',
+    'region' : 'REGION',
+    'subregion' : 'SUBREGION',
+    'lon' : 'LON',
+    'lat' : 'LAT',
+    'geom' : 'MULTIPOLYGON',
+}
+
